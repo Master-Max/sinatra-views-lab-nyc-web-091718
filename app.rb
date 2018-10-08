f145d6c443143e41ee408588ef530bcbf5bccb79
mx@E <mglxp5@gmail.com>
2018-10-08 15:18:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
+require 'date'
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get "/info" do
+		erb :info
+	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		@time = DateTime.now
+		erb :date
+	end
 
 end
